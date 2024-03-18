ead032268d28c6b96742dd55e26e937c6699cdc1
atefetasa <Atefe1376tasa613@gmal.com>
2024-03-18 19:50:00 +0330

    SOME CHANGES IN EMAIL_MODULE

--- email_module/forms.py
+++ email_module/forms.py
@@ -5,10 +5,6 @@ from ckeditor.widgets import CKEditorWidget
 from account_module.models import Signature
 
 
-class CustomFileField(forms.FileField):
-    pass
-
-
 class CreateEmailForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
@@ -97,3 +93,64 @@ class CreateEmailForm(forms.ModelForm):
                 raise forms.ValidationError("file size must be at most 25 Megabytes")
 
         return file
+
+    def clean_direct_receivers(self):
+        direct_receivers_raw_data = self.cleaned_data.get("direct_receivers")
+        if not direct_receivers_raw_data:
+            return direct_receivers_raw_data
+
+        selected_direct_receivers_list = direct_receivers_raw_data.split(',')
+        if selected_direct_receivers_list[-1] == '':
+            selected_direct_receivers_list.pop(-1)
+
+        return selected_direct_receivers_list
+
+    def clean_cc_receivers(self):
+        cc_receivers_raw_data = self.cleaned_data.get("cc_receivers")
+        if not cc_receivers_raw_data:
+            return cc_receivers_raw_data
+
+        selected_cc_receivers_list = cc_receivers_raw_data.split(',')
+        if selected_cc_receivers_list[-1] == '':
+            selected_cc_receivers_list.pop(-1)
+
+        return selected_cc_receivers_list
+
+    def clean_bcc_receivers(self):
+        bcc_receivers_raw_data = self.cleaned_data.get("bcc_receivers")
+        if not bcc_receivers_raw_data:
+            return bcc_receivers_raw_data
+
+        selected_bcc_receivers_list = bcc_receivers_raw_data.split(',')
+        if selected_bcc_receivers_list[-1] == '':
+            selected_bcc_receivers_list.pop(-1)
+
+        return selected_bcc_receivers_list
+
+    def clean(self):
+        cleaned_data = super().clean()
+        direct_receivers_set = set(cleaned_data.get("direct_receivers"))
+        cc_receivers_set = set(cleaned_data.get("cc_receivers"))
+        bcc_receivers_set = set(cleaned_data.get("bcc_receivers"))
+
+        if direct_receivers_set.intersection(cc_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        if direct_receivers_set.intersection(bcc_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        if cc_receivers_set.intersection(direct_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        if cc_receivers_set.intersection(bcc_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        if bcc_receivers_set.intersection(direct_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        if bcc_receivers_set.intersection(cc_receivers_set):
+            raise forms.ValidationError("to, cc and bcc fields should not contain same users.")
+
+        return cleaned_data
+
+
--- email_module/views.py
+++ email_module/views.py
@@ -2,8 +2,10 @@ from .models import *
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.views import View
 from .forms import *
 
@@ -14,7 +16,7 @@ class CreateMailView(LoginRequiredMixin, CreateView):
     model = Email
     form_class = CreateEmailForm
     template_name = 'email_module/compose_email.html'
-    success_url = reverse_lazy('home_page')
+    # success_url = reverse_lazy('home_page')
 
     def get_form_kwargs(self):
         kwargs = super(CreateMailView, self).get_form_kwargs()
@@ -43,44 +45,34 @@ class CreateMailView(LoginRequiredMixin, CreateView):
                 new_email.signature = selected_signature
 
             if selected_direct_receivers:
-                selected_direct_receivers_list = selected_direct_receivers.split(',')
-                if selected_direct_receivers_list[-1] == '':
-                    selected_direct_receivers_list.pop(-1)
-
-                for selected_direct_receiver in selected_direct_receivers_list:
+                for selected_direct_receiver in selected_direct_receivers:
                     to = User.objects.get(username__iexact=selected_direct_receiver)
                     if not to:
                         form.add_error('to', f'user with "{selected_direct_receiver}" username does not exist.')
-                        return self.form_invalid(form)
 
                     new_email.direct_receivers.add(to)
 
             if selected_cc_receivers:
-                selected_cc_receivers_list = selected_cc_receivers.split(',')
-                if selected_cc_receivers_list[-1] == '':
-                    selected_cc_receivers_list.pop(-1)
-
-                for selected_cc_receiver in selected_cc_receivers_list:
+                for selected_cc_receiver in selected_cc_receivers:
                     cc = User.objects.get(username__iexact=selected_cc_receiver)
                     if not cc:
                         form.add_error('cc', f'user with "{selected_cc_receiver}" username does not exist.')
-                        return self.form_invalid(form)
 
                     new_email.cc_receivers.add(cc)
 
             if selected_bcc_receivers:
-                selected_bcc_receivers_list = selected_bcc_receivers.split(',')
-                if selected_bcc_receivers_list[-1] == '':
-                    selected_bcc_receivers_list.pop(-1)
-
-                for selected_bcc_receiver in selected_bcc_receivers_list:
+                for selected_bcc_receiver in selected_bcc_receivers:
                     bcc = User.objects.get(username__iexact=selected_bcc_receiver)
                     if not bcc:
                         form.add_error('bcc', f'user with "{selected_bcc_receiver}" username does not exist.')
-                        return self.form_invalid(form)
 
                     new_email.bcc_receivers.add(bcc)
 
-            return super(CreateMailView, self).post(request, *args, **kwargs)
+            new_email.save()
 
-        return super(CreateMailView, self).form_invalid(form)
+            if draft:
+                return HttpResponseRedirect(reverse_lazy('home_page') + '?success=draft')
+            elif not draft:
+                return HttpResponseRedirect(reverse_lazy('home_page') + '?success=sent')
+
+        return render(request, self.template_name, {'form': form})
